d3481e5227f08221c3e68e5a5098908b67d38d3b
sarat <sarat@sarat-laptop>
2018-04-04 17:38:39 -0400

    commit Midterm Coding Assignment

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Semester.java
@@ -0,0 +1,10 @@
+package com.cisc181.core;
+
+import java.util.UUID;
+
+public class Semester {
+	private UUID SemesterID;
+	private date StartDate;
+	private date EndDate;
+
+}
---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Student.java
@@ -0,0 +1,44 @@
+package com.cisc181.core;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.cisc181.eNums.eMajor;
+
+public class Student extends Person {
+
+	private String eMajor;
+	private UUID StudentID;
+	
+	public String getMajor ( )
+    {
+        return this.eMajor;
+    }
+    public void setMajor (String Major)
+    {
+        this.eMajor = Major;    
+    }
+    
+    public UUID getStudentID(){
+    	return this.StudentID;
+    }
+    
+	public Student(String FirstName, String MiddleName, String LastName,Date DOB, String Major,
+			String Address, String Phone_number, String Email)
+	{
+		super(FirstName, MiddleName, LastName, DOB, Address, Phone_number, Email);
+		this.StudentID = UUID.randomUUID();
+		this.Major = Major;
+		
+	}
+	
+	@Override
+	public void PrintName() {
+		System.out.println(getLastName() + ","  + getFirstName() + ' ' + getMiddleName());
+	}
+
+	public void PrintName(boolean bnormal)
+	{
+		super.PrintName();
+	}
+}
